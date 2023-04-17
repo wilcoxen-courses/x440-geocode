eb4549681ae12aeeb1768dd1c2382b28e5556a35
pjwilcoxen <wilcoxen@syr.edu>
2023-04-16 20:27:12 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,29 +1,23 @@
-# Exercise: TBD
+# Example: Geocoding
 
 ## Summary
 
-TBD
+The **demo.py** file in this repository shows how to use the Nominatim API to geocode addresses. Nominatium is the search engine associated with OpenStreetMap.
 
 ## Input Data
 
-TBD 
+There are no input files: the addresses to be geocoded are built into **demo.py**. In a production version, they could be read from an input file instead.
 
 ## Deliverables
 
-TBD 
+None. This is an example only and there's nothing due.
 
 ## Instructions
 
-1. TBD
+1. Run demo.py and have a look at the PNG file it produces.
 
-1. TBD
-
-## Submitting
-
-Once you're happy with everything and have committed all of the changes to
-your local repository, please push the changes to GitHub. At that point, 
-you're done: you have submitted your answer.
+1. Load `demo.gpkg` into QGIS and have a look at it there as well.
 
 ## Tips
 
-+ TBD
+* Nominatium often returns multiple records for each search since there may be several things near the address. Each result has a detailed JSON object that describes what it is. If you just need the coordinates, it may be sufficient to pick any one of the records since the differences in the coordinates will be small.
--- demo.py
+++ demo.py
@@ -1,5 +1,108 @@
-'''
-Demonstrate TBD.
+"""
+nominatim.py
 Spring 2022 PJW
-'''
 
+Query Nominatim, the OpenStreetMap geocoding API.
+
+Note that Nominatium often returns several results for known objects
+near each address. If only the coordinates are needed, it should be
+sufficient to pick any one. However, considerable additional detail
+is available in the JSON object for each match.
+"""
+
+import requests
+import json
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+wgs84 = 4326
+
+#
+#  List of addresses to geocode
+#
+
+addresses = [
+    "1600 pennsylvania ave nw, washington, dc",
+    "734 S Crouse Ave, Syracuse, NY",
+    "1 Infinite Loop, Cupertino, CA",
+    "2700 W Anderson Ln., Austin, TX",
+    "440 Terry Ave N, Seattle, WA"
+    ]
+
+#
+#  Endpoint of the API
+#
+
+api = "https://nominatim.openstreetmap.org/search"
+
+#
+#  Loop through the addresses and build a list of results. Note
+#  that there are usually multiple hits per address. Will need to
+#  pick out the correct one by hand.
+#
+
+output = []
+
+for a in addresses:
+
+    #  Build the payload and make the query
+
+    payload = { 'q':f'<{a}>', 'format':'json' }
+
+    response = requests.get(api,payload)
+    assert response.status_code == 200
+
+    #  Parse the result
+
+    result = response.json()
+
+    #  Print it for reference
+
+    print( json.dumps(result, indent=4) )
+
+    #  Pull out the key information and append it to the list
+
+    for r in result:
+        newaddr = {
+            'query':a,
+            'name':r['display_name'],
+            'lat':r['lat'],
+            'lon':r['lon']
+            }
+        output.append(newaddr)
+
+#%%
+#
+#  Build a dataframe and write it out
+#
+
+adds = pd.DataFrame(output)
+adds.to_csv('addresses.csv',index=False)
+
+#%%
+#
+#  Read a US map for reference
+#
+
+us = gpd.read_file('locator_map.gpkg',layer='us')
+us.to_file('demo.gpkg',layer='us')
+
+#
+#  Build a GeoDataFrame of the geocoded points
+#
+
+geom = gpd.points_from_xy(adds['lon'], adds['lat'])
+geo = gpd.GeoDataFrame(data=adds, geometry=geom, crs=wgs84)
+geo = geo.to_crs(us.crs)
+geo.to_file('demo.gpkg',layer='addresses')
+
+#
+#  Now draw a quick map
+#
+
+fig,ax = plt.subplots(dpi=300)
+us.boundary.plot(color='black', linewidth=0.4, ax=ax)
+geo.plot(color='red', marker='D', markersize=20, ax=ax)
+ax.axis('off')
+fig.savefig('demo.png')
